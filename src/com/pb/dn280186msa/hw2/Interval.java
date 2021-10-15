2aec1f7cd21bf6951d99d0ad530586dd27e2ad87
dn280186msa <maksimov.msa@gmail.com>
2021-10-15 23:52:44 +0300

    init commit

--- src/com/pb/dn280186msa/hw2/Interval.java
+++ src/com/pb/dn280186msa/hw2/Interval.java
@@ -12,21 +12,21 @@ public class Interval {
         System.out.println("Введите число №1");
         operand1 = scan.nextInt();
 
-        if (operand1 >= 0 && operand1 <= 14) {
-        System.out.println("Число №1 входит в диапазон [0 - 14]");
+         if (operand1 >= 0 && operand1 <= 14) {
+                System.out.println("Число №1 входит в диапазон [0 - 14]");
+            }
+            else if (operand1 >= 15 && operand1 <= 35) {
+                System.out.println("Число №1 входит в диапазон [15 - 35]");
+            }
+            else if (operand1 >= 36 && operand1 <= 50) {
+                System.out.println("Число №1 входит в диапазон [36 - 50]");
+            }
+            else if (operand1 >= 51 && operand1 <= 100) {
+                System.out.println("Число №1 входит в диапазон [51 - 100]");
+            }
+        else {
+            System.out.println("Число №1 не входит в диапазоны");
         }
-        if (operand1 >= 15 && operand1 <= 35) {
-            System.out.println("Число №1 входит в диапазон [15 - 35]");
-        }
-        if (operand1 >= 36 && operand1 <= 50) {
-            System.out.println("Число №1 входит в диапазон [36 - 50]");
-        }
-        if (operand1 >= 51 && operand1 <= 100) {
-            System.out.println("Число №1 входит в диапазон [51 - 100]");
-        }
-        else{System.out.println("Число №1 не входит в диапазоны");}
-
-
-}
+    }
 
 }
