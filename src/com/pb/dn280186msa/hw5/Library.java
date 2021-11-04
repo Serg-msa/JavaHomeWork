5859c225fcb6f4e7d5f44cef485c2193844f474b
dn280186msa <maksimov.msa@gmail.com>
2021-11-04 22:43:38 +0200

    init commit

--- src/com/pb/dn280186msa/hw5/Library.java
+++ src/com/pb/dn280186msa/hw5/Library.java
@@ -11,11 +11,17 @@ public class Library {
         booksArray[2] = new Book("Энциклопедия", "Гусев К. В.", 2010);
 
         readersArray[0] = new Reader("Петров В. В.", 123456, "Инж", "20.01.2001", 123546);
-        readersArray[1] = new Reader("Петров В. В.", 123456, "Инж", "20.01.2001", 123546);
-        readersArray[2] = new Reader("Петров В. В.", 123456, "Инж", "20.01.2001", 123546);
+        readersArray[1] = new Reader("Иванов В. В.", 123456, "Инж", "20.01.2001", 123546);
+        readersArray[2] = new Reader("Сидоров В. В.", 654321, "Кку", "20.01.1969", 123546);
 
         readersArray[0].takeBook(3);
         readersArray[0].takeBook("Приключения","Словарь","Энциклопедия");
+        readersArray[0].takeBook(booksArray[0], booksArray[1]);
+
+        readersArray[2].returnBook(5);
+        readersArray[2].returnBook("Приключения","Словарь","Энциклопедия");
+        readersArray[2].returnBook(booksArray[2], booksArray[1]);
+
 
         for (int i = 0; i < readersArray.length; i++)
         {
--- src/com/pb/dn280186msa/hw5/Reader.java
+++ src/com/pb/dn280186msa/hw5/Reader.java
@@ -1,6 +1,12 @@
 package com.pb.dn280186msa.hw5;
 
+import com.pb.dn280186msa.hw3.Array;
+
+import java.util.Arrays;
+
 public class Reader {
+    Book[] booksArray = new Book[3];
+
     String fio;
     int nom;
     String fakul;
@@ -15,17 +21,50 @@ public class Reader {
         this.tel = tel;
     }
 
-    public void takeBook (int kol) {
-        System.out.println("Петров В. В. взял " + kol + " книги");
+
+
+        public void takeBook (int kol) {
+            System.out.println("Петров В. В. взял " + kol + " книги");
+        }
+
+        String takegg = "";
+        public void takeBook (String... books) {
+                    for (String book: books) {
+                        takegg += book + "; ";
+            }
+            System.out.println("Петров В. В. взял книги " + takegg);
+        }
+
+    String takeggg = "";
+    public void takeBook (Book... books) {
+        for (Book book: books)    {
+            takeggg += book.getInfo() + "; ";
+            }
+        System.out.println("Петров В. В. взял книги " + takeggg);
     }
 
-    String gg = "";
-    public void takeBook (String... books) {
-                for (String book: books) {
-               gg += book + "; ";
-        }
-        System.out.println("Петров В. В. взял книги " + gg);
+
+    public void returnBook (int kol) {
+        System.out.println("Сидоров В. В. вернул " + kol + " книги");
     }
+
+    String returgg = "";
+    public void returnBook (String... books) {
+        for (String book: books) {
+            returgg += book + "; ";
+        }
+        System.out.println("Сидоров В. В. вернул книги " + returgg);
+    }
+
+    String returggg = "";
+    public void returnBook (Book... books) {
+        for (Book book: books)    {
+            returggg += book.getInfo() + "; ";
+        }
+        System.out.println("Сидоров В. В. вернул книги " + returggg);
+    }
+
+
 
     String getInfo() {
         return "ФИО: " + fio + ", Номер читательского билета: " + nom
